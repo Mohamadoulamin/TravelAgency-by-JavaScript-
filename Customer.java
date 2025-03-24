2e42c8ca32b9829a22d8f80844032c9db837535a
Mohamadoulamin <163371259+Mohamadoulamin@users.noreply.github.com>
2025-03-24 22:43:43 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,15 @@
+class Customer {
+    private String customerID;
+    private String name;
+    private String email;
+    private String phone;
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
